fc7fdc309644a2af7aee58a5b160f598b56371d3
Arturs Jefimovs <arturs.jefimovs@b2broker.com>
2025-05-08 16:31:20 +0300

    Block sturcture

--- edit.sh
+++ edit.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-vim -p Parser.Mod Table.Mod Tree.Mod Builder.Mod Examples/Example.Mod Machine.Mod MocErrors.Mod Scanner.Mod
+vim -p Parser.Mod Builder.Mod Table.Mod Tree.Mod Examples/Example.Mod Machine.Mod MocErrors.Mod Scanner.Mod
