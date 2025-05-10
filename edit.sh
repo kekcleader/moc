185d85d50dd3cb294c3b733d63d3c62f6607018a
Arturs Jefimovs <arturs.jefimovs@b2broker.com>
2025-05-10 18:48:52 +0300

    Symbol table

--- edit.sh
+++ edit.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 vim -p Parser.Mod Builder.Mod Table.Mod Tree.Mod Examples/Example.Mod Machine.Mod MocErrors.Mod Scanner.Mod
+#vim -p Traverser.Mod Emitter.Mod Generator.Mod Parser.Mod Builder.Mod Table.Mod Tree.Mod Examples/Example.Mod Machine.Mod MocErrors.Mod Scanner.Mod
