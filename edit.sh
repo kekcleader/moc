6052bc94929e9378a703ebd80b4521f0c365500a
Arturs Jefimovs <arturs.jefimovs@b2broker.com>
2025-05-10 22:02:13 +0300

    Pseudoimport

--- edit.sh
+++ edit.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-vim -p Parser.Mod Builder.Mod Table.Mod Tree.Mod Examples/Example.Mod Machine.Mod MocErrors.Mod Scanner.Mod
+vim -p Examples/Example.Mod Parser.Mod Builder.Mod Table.Mod Tree.Mod Machine.Mod MocErrors.Mod Scanner.Mod
 #vim -p Traverser.Mod Emitter.Mod Generator.Mod Parser.Mod Builder.Mod Table.Mod Tree.Mod Examples/Example.Mod Machine.Mod MocErrors.Mod Scanner.Mod
