0eb73cead88567bc326038ecc2bacfa720d8a22e
Arturs Jefimovs <arturs.jefimovs@b2broker.com>
2025-05-11 03:52:56 +0300

    Traverser

--- edit.sh
+++ edit.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-vim -p Examples/Example.Mod Parser.Mod Builder.Mod Table.Mod Tree.Mod Machine.Mod MocErrors.Mod Scanner.Mod
-#vim -p Traverser.Mod Emitter.Mod Generator.Mod Parser.Mod Builder.Mod Table.Mod Tree.Mod Examples/Example.Mod Machine.Mod MocErrors.Mod Scanner.Mod
+#vim -p Examples/Example.Mod Parser.Mod Builder.Mod Table.Mod Tree.Mod Machine.Mod MocErrors.Mod Scanner.Mod
+vim -p Traverser.Mod Emitter.Mod Generator.Mod Table.Mod Tree.Mod Examples/Example.Mod MocErrors.Mod Linker.Mod
+
