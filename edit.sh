3e96555952ab82984d6e455e38184fecd31195fd
Arturs Jefimovs <arturs.jefimovs@b2broker.com>
2025-05-26 12:58:19 +0300

    Generator

--- edit.sh
+++ edit.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #vim -p Examples/Example.Mod Parser.Mod Builder.Mod Table.Mod Tree.Mod Machine.Mod MocErrors.Mod Scanner.Mod
-vim -p Traverser.Mod Emitter.Mod Generator.Mod Table.Mod Tree.Mod Examples/Example.Mod MocErrors.Mod Linker.Mod
+vim -p Parser.Mod Scanner.Mod Traverser.Mod Emitter.Mod Generator.Mod Table.Mod Tree.Mod Examples/Example.Mod MocErrors.Mod Linker.Mod
 
